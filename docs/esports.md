28bbf3feaa4a613e0de2d4a37b93e8263b3b9a0d
aistha11 <bijayshrestha0908@gmail.com>
2021-11-05 15:24:55 +0545

    Doc only mode

---
+++ docs/esports.md
@@ -0,0 +1,11 @@
+---
+id: esports
+title: E-Sports
+sidebar_label: esports
+---
+
+## E-Sports
+
+Documenting the e-sport project
+
+---
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Ai Docs',
+  tagline: 'A docummenting tuto',
+  url: 'https://aistha11.github.io',
+  baseUrl: '/aidocs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'aistha11', // Usually your GitHub org/user name.
+  projectName: 'aidocs', // Usually your repo name.
 
   presets: [
     [
@@ -23,14 +23,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+          editUrl: 'https://github.com/aistha11/aidocs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -43,21 +38,14 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Ai Docs',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Ai Docs Logo',
           src: 'img/logo.svg',
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/aistha11/aidocs/',
             label: 'GitHub',
             position: 'right',
           },
@@ -71,7 +59,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
@@ -79,16 +67,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/aistha11',
               },
             ],
           },
@@ -96,12 +76,8 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/aistha11/aidocs',
               },
             ],
           },
